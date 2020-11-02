3f622f1ad415457e536eb2a6e82df3aa157738b7
Joel Estrada <jre.estrada@gmail.com>
2020-11-02 15:23:37 -0800

    added quetsions to questions array

--- Assets/index.js
+++ Assets/index.js
@@ -2,8 +2,31 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // array of questions for user
-const questions = [
-
+const questions = [{
+        type: "input",
+        message: "Hi! What is your project name?",
+        name: "projectName"
+    },
+    {
+        type: "input",
+        message: "Please write a short description of your project.",
+        name: "projectDescription"
+    },
+    {
+        type: "input",
+        message: "What kind of license should your project have?",
+        name: "projectLicense"
+    },
+    {
+        type: "input",
+        message: "What is your GitHub URL?",
+        name: "userGitHub"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "userEmail"
+    }
 ];
 
 // function to write README file
